764744d96987238302ff76c522651d7fd0e8a33b
codewitch-honey-crisis <gaztoast@gmail.com>
2024-01-24 09:13:51 -0800

    improved demo

--- Example/Program.cs
+++ Example/Program.cs
@@ -24,20 +24,20 @@ static partial class Program
 	public static List<int> Indices = null;
 	static void Run()
 	{
-		//Console.Write("Progress test: ");
-		//for (int i = 0; i < 10; ++i)
-		//{
-		//	WriteProgress(i, i > 0, Console.Out);
-		//	Thread.Sleep(100);
-		//}
-		//Console.WriteLine();
-		//Console.Write("Progress bar test: ");
-		//for (int i = 0; i <= 100; ++i)
-		//{
-		//	WriteProgressBar(i, i > 0, Console.Out);
-		//	Thread.Sleep(10);
-		//}
-		//Console.WriteLine();
+		Console.Write("Progress test: ");
+		for (int i = 0; i < 10; ++i)
+		{
+			WriteProgress(i, i > 0, Console.Out);
+			Thread.Sleep(100);
+		}
+		Console.WriteLine();
+		Console.Write("Progress bar test: ");
+		for (int i = 0; i <= 100; ++i)
+		{
+			WriteProgressBar(i, i > 0, Console.Out);
+			Thread.Sleep(10);
+		}
+		Console.WriteLine();
 		Console.WriteLine(WordWrap("fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales ut eu sem integer vitae justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor",40,0));
 		
 	}
